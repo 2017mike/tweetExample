7d46d72086935fb493008049e8d015c52211c265
Michael Scharf <mwscharf@ucdavis.edu>
2022-06-27 18:46:40 -0700

    added comment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router()
 const getTweet = require('../api')
 
+
+//get single tweet
 router.get('/tweet', async(req, res) => {
   const tweet =  await getTweet()
   res.json(tweet)
